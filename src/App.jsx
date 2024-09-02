f7cfc7f76f443fd6812d968ed987abc888e3ab8a
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-09-02 19:31:17 +0700

    bismillah, initial project

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Heading,
+  Divider,
+  Center,
+  Button,
+  Input,
+  Spinner,
+  useToast,
+} from "@chakra-ui/react";
+import Student from "./Student";
+
+function App() {
+  const [nim, setNim] = useState("");
+  const [student, setStudent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const apiBaseUrl = "https://my-umby-profile-api.vercel.app";
+  const toast = useToast();
+
+  function handleSearch() {
+    if (!nim) {
+      setStudent(null);
+      return false;
+    }
+
+    setIsLoading(true);
+
+    fetch(`${apiBaseUrl}/student/${nim}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setStudent(data.result);
+
+        toast({
+          title: "Successfully.",
+          description: "Successfully retrieve student data for you.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+
+        toast({
+          title: "Failed.",
+          description: "Failed to retrieve student data for you.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      w="100vw"
+      h="100vh"
+      flexDir="column"
+    >
+      <Card width="xl">
+        <CardBody>
+          <Center>
+            <Heading size="md" my={2}>
+              My UMBY Profile
+            </Heading>
+          </Center>
+          <Divider mt={2} mb={2} />
+          <Box display="flex" gap={2}>
+            <Input
+              placeholder="Type Your NIM"
+              value={nim}
+              onChange={({ target }) => setNim(target.value)}
+            />
+            <Button variant="solid" colorScheme="blue" onClick={handleSearch}>
+              Search
+            </Button>
+          </Box>
+          {isLoading ? (
+            <Center>
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+                mt={3}
+              />
+            </Center>
+          ) : (
+            student && <Student student={student} />
+          )}
+        </CardBody>
+      </Card>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/Student.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import {
+  Stack,
+  Divider,
+  Center,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableContainer,
+  Avatar,
+  Text,
+} from "@chakra-ui/react";
+
+export default function Student({ student }) {
+  const { pict_url, ...rest } = student;
+
+  return (
+    <>
+      <Divider my={2} />
+      <Center>
+        <Avatar src={pict_url} alt={rest.name} size="2xl" />
+      </Center>
+      <Stack mt="6" spacing="3">
+        <TableContainer>
+          <Table variant="simple">
+            <Tbody>
+              {Object.keys(rest).map((s) => {
+                return (
+                  <Tr key={s}>
+                    <Td>
+                      <Text
+                        css={{ fontWeight: "bold" }}
+                        _firstLetter={{ textTransform: "uppercase" }}
+                      >
+                        {s}:
+                      </Text>
+                    </Td>
+                    <Td>
+                      <Text align="right">{rest[s]}</Text>
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Stack>
+    </>
+  );
+}
